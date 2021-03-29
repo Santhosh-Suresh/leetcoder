92a7d9d8ed869c596f8be4e7df1bfe8b252edacf
Santhosh-Suresh <sssandy@umich.edu>
2021-03-29 22:49:19 +0530

    Solve Sum of Left Leaves (Easy) via Recursion model

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-from Matrix_01_542 import Solution
+from LeetCode.sum_of_left_leaves import Solution
 
 a = Solution()
-
-nums = [[0,0,0],   [0,1,0],  [1,1,1]]
-print(a.updateMatrix(nums))
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# from LeetCode.Binary_tree import TreeNode, Solution
-#
-# a = TreeNode(2)
-# b = TreeNode(5)
-# c= TreeNode(6)
-# d = TreeNode(8)
-# e = TreeNode(10)
-# f = TreeNode(11)
-# g = TreeNode(12)
-#
-# a.left = d
-# a.right = e
-#
-# b.left = f
-# b.right = g
-#
-# c.left = a
-# c.right = b
-#
-# search = Solution()
-# ans = search.bfs(c, 22)
-# print(ans, ans.val if ans else None)
